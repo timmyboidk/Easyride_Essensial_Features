c2a57eece058b748fb06c915a443441e2726e5ed
Tim Zaiyang Yu <zaiyangyu369@gmail.com>
2025-01-04 16:59:50 -0500

    update user_service directory

--- user_service/src/main/java/com/easyride/user_service/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.easyride.user_service.service;
-
-import com.easyride.user_service.dto.UserRegistrationDto;
-
-public interface UserService {
-
-    void registerUser(UserRegistrationDto registrationDto);
-}
